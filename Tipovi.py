a1468076c11ec603e4c3b6e66c7b56d9396288d7
Miloš Đorđević <midj24@outlook.com>
2025-11-24 19:33:48 +0100

    Vezbanje tipova podataka, i deklaracija istih.

---
+++ Tipovi.py
@@ -0,0 +1,9 @@
+grad = "Beograd" #string
+temperatura = 4.5 #float
+godina = 2025 #int
+susnezica = False #True ili False (Bool-Logicki tip)
+
+print("Grad: ", grad)
+print("Temperatura: ", temperatura,"C")
+print("Godina:", godina)
+print("Pada sneg:", susnezica)
